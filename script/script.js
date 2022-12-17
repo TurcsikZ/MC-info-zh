9d5b02f833f26f002b86c6d7b7f3944b3b407f60
unknown <dzsozi2@gmail.com>
2022-12-17 14:16:06 +0100

    uploading html, css and script files, plus pictures for excercise 1

---
+++ script/script.js
@@ -0,0 +1,24 @@
+function changeImage()
+            {
+            var img = document.getElementById("kep");
+            img.src="../img/math5.jpg";
+            return false;
+            }
+            function changeImage2()
+            {
+            var img = document.getElementById("kep");
+            img.src="../img/math4.jpg";
+            return false;
+            }
+            function changeImage3()
+            {
+            var img = document.getElementById("kep");
+            img.src="../img/math3.jpg";
+            return false;
+            }
+            function changeImage4()
+            {
+            var img = document.getElementById("kep");
+            img.src="../img/math2.jpg";
+            return false;
+            }
